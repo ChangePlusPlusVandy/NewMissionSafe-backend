dba4784e836b4542fd53d97d78dc847a81e46655
txingxie <trevorxingxie@gmail.com>
2023-11-12 17:09:58 -0600

    adds preliminary staff controller and router

--- src/controllers/staff.controller.ts
+++ src/controllers/staff.controller.ts
@@ -1,1 +1,53 @@
-import { StaffModel } from "../models/staff";
+import { StaffModel, staffType } from "../models/staff";
+
+export const createStaff = async (staff: staffType) => {
+  const newStaff = new StaffModel(staff);
+  await newStaff.save();
+  return newStaff;
+};
+
+export const updateStaff = async (
+  firebaseUID: string,
+  update: { key: string; value: string },
+) => {
+  const updateObj = { $set: { [update.key]: update.value } };
+  const staff = await StaffModel.findOneAndUpdate({ firebaseUID }, updateObj);
+  if (!staff) throw new Error("Staff not found");
+};
+
+export const getAllStaff = async () => {
+  const allStaff = await StaffModel.find({});
+  if (!allStaff) throw new Error("No staff found");
+  return allStaff;
+};
+
+export const getStaffByEmail = async (email: string) => {
+  const staff = await StaffModel.findOne({ email });
+  if (!staff) throw new Error("Staff not found");
+  return staff;
+};
+
+export const getStaffByID = async (firebaseUID: string) => {
+  const staff = await StaffModel.findOne({ firebaseUID });
+  if (!staff) throw new Error("Staff not found");
+  return staff;
+};
+
+export const getStaffByActive = async () => {
+  const staff = await StaffModel.find({ active: true });
+  if (!staff) throw new Error("Staff not found");
+  return staff;
+};
+
+export const getStaffByInactive = async () => {
+  // Unneeded? Router just uses getStaffByActive(active: false)
+  const staff = await StaffModel.find({ active: false });
+  if (!staff) throw new Error("Staff not found");
+  return staff;
+};
+
+export const getStaffByProgram = async (programs: string[]) => {
+  const staff = await StaffModel.find({ programs });
+  if (!staff) throw new Error("Staff not found");
+  return staff;
+};
--- src/routers/staff.router.ts
+++ src/routers/staff.router.ts
@@ -1,3 +1,107 @@
-import { Router } from "express";
+import { Router, json } from "express";
+
+const {
+  createStaff,
+  updateStaff,
+  getAllStaff,
+  getStaffByEmail,
+  getStaffByID,
+  getStaffByActive,
+  getStaffByInactive,
+  getStaffByProgram,
+} = require("../controllers/staff.controller");
 
 export const staffRouter = Router();
+staffRouter.use(json());
+
+// POST add new Staff document
+staffRouter.post("/", async (req, res) => {
+  try {
+    let newStaff = await createStaff(req.body);
+    res.status(201).send(newStaff);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// GET all Staff documents
+staffRouter.get("/", async (_req, res) => {
+  try {
+    let allStaff = await getAllStaff();
+    res.status(200).send(allStaff);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
+// GET staff by email
+staffRouter.get("/byEmail/:email", async (req, res) => {
+  try {
+    let staff = await getStaffByEmail(req.params.email);
+    res.status(200).send(staff);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
+// GET Staff document matching param fireID
+staffRouter.get("/byID/:firebaseUID", async (req, res) => {
+  try {
+    let staff = await getStaffByID(req.params.firebaseUID);
+    res.status(200).send(staff);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
+// GET active Staff
+staffRouter.get("/active", async (_req, res) => {
+  try {
+    let activeStaff = await getStaffByActive(true);
+    res.status(200).send(activeStaff);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
+// GET inactive Staff
+staffRouter.get("/inactive", async (_req, res) => {
+  try {
+    let inactiveStaff = await getStaffByActive(false);
+    res.status(200).send(inactiveStaff);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
+// GET staff by email
+staffRouter.get("/byPrograms/:programs", async (req, res) => {
+  try {
+    let staff = await getStaffByProgram(req.params.programs);
+    res.status(200).send(staff);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
+// PUT active to true for Staff document matching
+//     param fireID
+staffRouter.put("/activate/:firebaseUID", async (req, res) => {
+  try {
+    await updateStaff(req.params.firebaseUID, { active: true });
+    res.status(200).send("Staff activated");
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// PUT active to false for Staff document matching
+//     param fireID
+staffRouter.put("/deactivate/:firebaseUID", async (req, res) => {
+  try {
+    await updateStaff(req.params.firebaseUID, { active: false });
+    res.status(200).send("Staff deactivated");
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
