b2d0fba3317f09ab474b0002ae6cfbf1df9a17a0
samshefnie <samshefnie@gmail.com>
2024-05-14 03:04:01 -0400

    add phone for staff

--- src/models/staff.ts
+++ src/models/staff.ts
@@ -5,6 +5,7 @@ export interface staffType {
   firstName: string;
   lastName: string;
   email: string;
+  phone: string;
   firebaseUID: string;
   program: string;
   active: boolean;
@@ -15,6 +16,7 @@ const Staff = new Schema<staffType>({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, index: { unique: true } },
+  phone: { type: String, required: true },
   firebaseUID: { type: String, required: true, index: { unique: true } },
   program: { type: String, required: true },
   active: { type: Boolean, default: false },
